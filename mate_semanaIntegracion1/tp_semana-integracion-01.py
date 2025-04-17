4255934863f88209fe3d5a49c35b971245ff1194
Santiago Varela <121682758+santiagovOK@users.noreply.github.com>
2025-04-17 10:38:08 -0300

    Modificando comentarios del en tp_semana-integracion-01.py 2025-04-17

--- mate_semanaIntegracion1/tp_semana-integracion-01.py
+++ mate_semanaIntegracion1/tp_semana-integracion-01.py
@@ -1,30 +1,31 @@
-# Idea: Contador Binario
+# Idea que seleccionamos: Contador Binario / Última revisión: 2025-04-17
 
-# Última revisión: 2025-04-15
+# Escriban un programa que, usando un ciclo, cuente desde 0 hasta 15 y muestre cada número en su representación binaria. Extensión: Utilicen un retardo (por ejemplo, con time.sleep) para simular el conteo de un circuito.
 
-# Escriban un programa que, usando un ciclo, cuente desde 0 hasta 15 y muestre cada número en su representación binaria. 
-# Extensión: Utilicen un retardo (por ejemplo, con time.sleep) para simular el conteo de un circuito.
-import time #se importa la biblioteca time para agregar la funcion time.sleep().
+# --- 
+
+import time # Importamos la biblioteca time para agregar la funcion `time.sleep()`.
+
 for numero in range(0, 16):
-    time.sleep(1) #esta funcion pausa 1 seg la ejecucion del bucle.
+    # Usamos `time.sleep()` para pausar 1 seg. la ejecucion del bucle en cada iteración.
+    time.sleep(1)
     # Creamos una variable para almacenar el binario en tipo cadena
     binario = "" 
-    # Hacemos una copia para no modificar 'numero' del for
+    # Hacemos una copia para no modificar 'numero' del `for`
     n = numero
-    # creamos una condicional doble para agregar el 0.
+    # Creamos una condicional doble para agregar el 0.
     if numero == 0:
         print("0 en binario es 0")
     else:
+    # Dentro de la condicional, agregamos el `while` que incluye el pasaje de cada uno de los números del rango a binario.
         while n > 0:
+            # Calculamos el residuo de la división por 2
             residuo = n % 2
+            # Concatenamos el residuo al inicio de la cadena binaria.
             binario = str(residuo) + binario
+            # Actualizamos n dividiendo por 2.
             n = n // 2
+        # Imprimimos los resultados en cada iteración.    
         print(f"{numero} en binario es {binario}")
 
-
-# NOTAS
-
-# Faltaría incluir el 0 dentro del rango
-# Deberíamos usar time.sleep ya que lo señala la consigna misma
-# Si van a usar AI, traten de documentar más o menos qué es lo que hacen (y por qué necesitaban preguntar eso) 
-# así vemos como lo agregamos a la fundamentación
+# --- 
