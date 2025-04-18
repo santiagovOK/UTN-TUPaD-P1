39108b9ef65fb884653791719a16ac1dd0c90209
Santiago Varela <121682758+santiagovOK@users.noreply.github.com>
2025-04-18 15:38:04 -0300

    Inicio de VARELA_TP-06.py 2025-04-18

---
+++ 06DatosComplejos/VARELA_TP-06.py
@@ -0,0 +1,70 @@
+# Trabajo práctico Nº6 - Estructuras de datos complejas -  Varela Santiago Octavio (Comisión 22)
+
+# Resolución del trabajo práctico Nº6 al 2025-04-18
+
+# Respuestas también en https://github.com/santiagovOK/UTN-TUPaD-P1/tree/main/06DatosComplejos/VARELA_TP-06.ipynb
+
+# ----------------------------------------------------------------------
+
+# 1) Dado el diccionario precios_frutas 
+# precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
+
+# Añadir las siguientes frutas con sus respectivos precios:
+# ●​ Naranja = 1200
+# ●​ Manzana = 1500
+# ●​ Pera = 2300
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+# 2) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código desarrollado en el punto anterior, actualizar los precios de las siguientes frutas:
+# ●​ Banana = 1330
+# ●​ Manzana = 1700
+# ●​ Melón = 2800
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+# 3) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código desarrollado en el punto anterior, crear una lista que contenga únicamente las frutas sin los precios.
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+# 4) Crear una clase llamada Persona que contenga un método __init__ con los atributos nombre, pais y edad y el método saludar. El método saludar debe imprimir por pantalla un mensaje de salud que siga la estructura "¡Hola! Soy [nombre], vivo en [pais] y tengo [edad] años."
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+# 5) Crear una clase llamada Circulo que contenga el atributo radio y los métodos calcular_area y calcular_perimetro. Dichos métodos deben calcular el parámetro correspondiente. Ayuda: el módulo math puede ser de utilidad para usar la constante 𝜋.
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+# 6) Dado un string con paréntesis "()", "{}", "[]", verifica si están correctamente balanceados usando una pila. Ejemplo de entrada y salida: (ver en PDF)
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+# 7) Usa una cola para simular un sistema de turnos en un banco. La cola debe permitir:
+
+# ●​ Agregar clientes (encolar).
+# ●​ Atender clientes (desencolar).
+# ●​ Mostrar el siguiente cliente en la fila.
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+# 8) Crea una lista enlazada que permita insertar nodos al inicio y recorrer la lista para mostrar los valores almacenados.
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+# 9) Dada una lista enlazada, implementa una función para invertirla.Ejemplo de entrada y salida: (Ver en PDF)
