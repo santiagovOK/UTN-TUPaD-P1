bc302fed466842a8bd9ed52ea18f970462d27b98
Santiago Varela <121682758+santiagovOK@users.noreply.github.com>
2025-05-22 00:10:25 -0300

    Inicio de VARELA_TP-06-1.py 2025-05-22

---
+++ 06_1_Recursividad/VARELA_TP-06-1.py
@@ -0,0 +1,107 @@
+# Trabajo práctico Nº7 - Recursividad -  Varela Santiago Octavio (Comisión 22)
+
+# Resolución del trabajo práctico Nº7 al 2025-05-22
+
+# Respuestas también en https://github.com/santiagovOK/UTN-TUPaD-P1/tree/main/06_1_Recursividad/VARELA_TP-06-1.ipynb
+
+# ----------------------------------------------------------------------
+
+"""
+1. Crea una función recursiva que calcule el factorial de un número. Luego, utiliza esa
+función para calcular y mostrar en pantalla el factorial de todos los números enteros
+entre 1 y el número que indique el usuario.
+
+"""
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+"""
+2) Crea una función recursiva que calcule el valor de la serie de Fibonacci en la posición indicada. Posteriormente, muestra la serie completa hasta la posición que el usuario especifique.
+
+"""
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+"""
+3) Crea una función recursiva que calcule la potencia de un número base elevado a un exponente, utilizando la fórmula nm = n ∗ n(m−1). Prueba esta función en un algoritmo general.
+"""
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+"""
+4) Crear una función recursiva en Python que reciba un número entero positivo en base decimal y devuelva su representación en binario como una cadena de texto.
+"""
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+"""
+5) Implementá una función recursiva llamada es_palindromo(palabra) que reciba una cadena de texto sin espacios ni tildes, y devuelva True si es un palíndromo o False si no lo es.
+
+Requisitos:
+La solución debe ser recursiva.
+No se debe usar [::-1] ni la función reversed().
+"""
+
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+
+"""
+6) Escribí una función recursiva en Python llamada suma_digitos(n) que reciba un
+número entero positivo y devuelva la suma de todos sus dígitos.
+
+Restricciones:
+
+No se puede convertir el número a string.
+Usá operaciones matemáticas (%, //) y recursión.
+
+Ejemplos:
+suma_digitos(1234) → 10 (1 + 2 + 3 + 4)
+suma_digitos(9) → 9
+suma_digitos(305) → 8 (3 + 0 + 5)
+"""
+
+# ----------------------------------------------------------------------
+
+"""
+7) Un niño está construyendo una pirámide con bloques. En el nivel más bajo coloca n
+bloques, en el siguiente nivel uno menos (n - 1), y así sucesivamente hasta llegar al
+último nivel con un solo bloque.
+
+Escribí una función recursiva contar_bloques(n) que reciba el número de bloques en el
+nivel más bajo y devuelva el total de bloques que necesita para construir toda la
+pirámide.
+
+Ejemplos:
+contar_bloques(1) → 1
+(1)
+contar_bloques(2) → 3
+(2 + 1)
+contar_bloques(4) → 10
+(4 + 3 + 2 + 1)
+
+"""
+# ----------------------------------------------------------------------
+
+"""
+8) Escribí una función recursiva llamada contar_digito(numero, digito) que reciba un
+número entero positivo (numero) y un dígito (entre 0 y 9), y devuelva cuántas veces
+aparece ese dígito dentro del número.
+
+Ejemplos:
+
+contar_digito(12233421, 2) → 3
+contar_digito(5555, 5) →4
+contar_digito(123456, 7) →0
+"""
+
+# ----------------------------------------------------------------------
+
